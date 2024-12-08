346270b175cb1450fc5b2bebd47694dca18388cd
Miguel Rodríguez González <uo263494@uniovi.es>
2024-12-08 18:47:37 +0100

    feat: day 6 success

--- 6day/main.ts
+++ 6day/main.ts
@@ -1,3 +1,18 @@
 function inBox(box: string[]): boolean {
+    const REGEX = /^#[^#]*\*[^#]*#$/;
+
+    for (let i = 1; i < box.length - 1; i++) {
+        const present = box[i];
+        if (present.match(REGEX)) {
+            return true;
+        }
+    }
     return false;
 }
+
+console.log(inBox(['###', '#*#', '###'])); // ➞ true
+console.log();
+console.log(inBox(['####', '#* #', '#  #', '####'])); // ➞ true
+console.log();
+console.log(inBox(['#####', '#   #', '#  #*', '#####'])); // ➞ false
+//
